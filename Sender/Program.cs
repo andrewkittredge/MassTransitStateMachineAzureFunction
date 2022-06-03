762f5eb263635562c1e133a3e91090bb14bf6c87
Andrew Kittredge <andrewlkittredge@gmail.com>
2022-06-03 15:52:52 -0400

    working on sender

---
+++ Sender/Program.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sample.AzureFunction;
+using Sample.AzureFunction.Consumers;
+
+namespace Sender
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args);
+            builder.ConfigureServices((_, services) =>
+            {
+                services.AddMassTransitForAzureFunctions(
+                    cfg =>
+                    {
+                        cfg.AddConsumersFromNamespaceContaining<ConsumerNamespace>();
+                        cfg.AddRequestClient<SubmitOrder>(new Uri("queue:getting-started"));
+                    },
+                    configureBus: (x, configureBus) => configureBus.Host("Endpoint=sb://cbandrewtest.servicebus.windows.net/;SharedAccessKeyName=MassTransit;SharedAccessKey=fD4VhRMToqsTBpfdyjAH1nqC7xIkqspjHaJUP9cdELQ="));
+                services.AddHostedService<Worker>();
+            });
+            return builder;
+        }
+    }
+}
---
+++ Sender/Worker.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Sample.AzureFunction;
+
+namespace Sender
+{
+    public class Worker: BackgroundService
+    {
+        readonly IRequestClient<SubmitOrder> _client;
+        readonly ILogger<Worker> _logger;
+
+        public Worker(IRequestClient<SubmitOrder> client, ILogger<Worker> logger)
+        {
+            _client = client;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //   var (message, _) = await MessageInitializerCache<ProcessPressRelease>.InitializeMessage(new { PressReleaseID = 1});
+            // var sendEndPoint = await
+            var response = await _client.GetResponse<OrderAccepted>(new { OrderId = 123 });
+            
+        }
+    }
+}
