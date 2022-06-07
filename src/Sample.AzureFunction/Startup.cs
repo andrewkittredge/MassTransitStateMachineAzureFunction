19758e87554cff1f825b535a76dcc7144de2e178
Andrew Kittredge <andrewlkittredge@gmail.com>
2022-06-07 14:04:32 -0400

    need to reference consumer

--- src/Sample.AzureFunction/Startup.cs
+++ src/Sample.AzureFunction/Startup.cs
@@ -20,6 +20,7 @@ namespace Sample.AzureFunction
                 .AddScoped<InitiatePressReleaseBatchFunction>()
                 .AddMassTransitForAzureFunctions(cfg =>
                     {
+                        cfg.AddConsumer<InitiatePressReleaseBatch>();
                         cfg.AddRequestClient<StartPressReleaseBatchFromSender>(new Uri("queue:getting-started"));
                         cfg.AddSagaStateMachine<PressReleaseBatchStateMachine, PressReleaseBatchState>(typeof(PressReleaseBatchStateMachineDefinition)).InMemoryRepository();
                     },
